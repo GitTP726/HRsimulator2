f614583291113c54f0a3b742063ebfd04cf526a3
Trang P <tuq28415@temple.edu>
2025-11-29 20:14:11 -0500

    8th attempt

--- server.js
+++ server.js
@@ -116,6 +116,6 @@ app.post("/return", (req, res) => {
 
 // ---------- Start Server ----------
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, "0.0.0.0", () =>
-  console.log(`💓 HR Simulator running on http://localhost:${PORT}`)
-);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`💓 HR Simulator running on port ${PORT}`)
+});
